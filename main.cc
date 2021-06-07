3f27e93eb5d9fe5addcba1aa92d9703c07084574
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-06 22:03:17 -0700

    simple program to turn the level shifter hat on

--- main.cc
+++ main.cc
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <wiringPi.h>
+constexpr auto EnablePin = 25;
 int main() {
+	wiringPiSetup();
+	pinMode(EnablePin, OUTPUT);
+	digitalWrite(EnablePin, HIGH);
 	std::cout << "chipset!" << std::endl;
+	for (int i = 0; i < 10; ++i) {
+		digitalWrite(EnablePin, LOW);
+		delay(1000);
+		digitalWrite(EnablePin, HIGH);
+		delay(1000);
+	}
+	digitalWrite(EnablePin, LOW);
 	return 0;
 }
 
