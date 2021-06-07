9e25d7754b00764828e07f1d8aa8b2d615bb9919
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-06 22:15:16 -0700

    setup some of the pinouts

--- main.cc
+++ main.cc
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <wiringPi.h>
+constexpr auto DENPin = 1;
+constexpr auto ASPin = 4;
+constexpr auto INT0Pin = 5;
+constexpr auto WRPin = 6;
+constexpr auto RESET960Pin = 26;
 constexpr auto LevelShifterHatEnable = 25;
-int main() {
-	wiringPiSetup();
+template<auto pin, auto assertState = LOW, auto deassertState = HIGH>
+struct PinHolder final {
+	PinHolder() {
+		digitalWrite(pin, assertState);
+	}
+~PinHolder() {
+	digitalWrite(pin, deassertState);
+}
+};
+void setup() {
+	pinMode(RESET960Pin, OUTPUT);
 	pinMode(LevelShifterHatEnable, OUTPUT);
+	PinHolder<RESET960Pin> holdi960InReset;
 	digitalWrite(LevelShifterHatEnable, HIGH);
 	std::cout << "chipset!" << std::endl;
-	for (int i = 0; i < 10; ++i) {
-		digitalWrite(LevelShifterHatEnable, LOW);
-		delay(1000);
-		digitalWrite(LevelShifterHatEnable, HIGH);
-		delay(1000);
+}
+int main() {
+	wiringPiSetup();
+	setup();
+	while (true) {
 	}
 	digitalWrite(LevelShifterHatEnable, LOW);
 	return 0;
