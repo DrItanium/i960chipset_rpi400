fbfe3a5e0a0a338f2fcc510edec02e030c55b6e4
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-06 22:19:31 -0700

    allocate ram space

--- main.cc
+++ main.cc
@@ -1,4 +1,6 @@
+#include <cstdint>
 #include <iostream>
+#include <memory>
 #include <wiringPi.h>
 constexpr auto DENPin = 1;
 constexpr auto ASPin = 4;
@@ -6,14 +8,19 @@ constexpr auto INT0Pin = 5;
 constexpr auto WRPin = 6;
 constexpr auto RESET960Pin = 26;
 constexpr auto LevelShifterHatEnable = 25;
+constexpr auto READYPin = -1;
+constexpr auto BLASTPin = -1;
+constexpr auto FAILPin = -1;
+constexpr auto GPIOCSPin = -1;
+union MemoryCell {
+	uint16_t value = 0;
+	uint8_t bytes[2];
+};
+std::unique_ptr<MemoryCell[]> ram;
 template<auto pin, auto assertState = LOW, auto deassertState = HIGH>
 struct PinHolder final {
-	PinHolder() {
-		digitalWrite(pin, assertState);
-	}
-~PinHolder() {
-	digitalWrite(pin, deassertState);
-}
+	PinHolder() { digitalWrite(pin, assertState); }
+	~PinHolder() { digitalWrite(pin, deassertState); }
 };
 void setup() {
 	pinMode(RESET960Pin, OUTPUT);
@@ -23,6 +30,7 @@ void setup() {
 	std::cout << "chipset!" << std::endl;
 }
 int main() {
+	ram = std::make_unique<MemoryCell[]>((512 * 1024 * 1024) / sizeof(MemoryCell));
 	wiringPiSetup();
 	setup();
 	while (true) {
